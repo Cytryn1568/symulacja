8edf3977b41e8e3b13e68f1f4f4b9eef4e800152
JGarwola <jakub.garwola@gmail.com>
2020-04-01 18:20:48 +0200

    symulacja

---
+++ tekst_tekstowy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 16:19:03 2020
+
+@author: User
+"""
+
+
+import symulacja
+
+pop = Populacja(n=7)
+print(pop)
+pop.ruch()
+print("------------")
+print(pop)
---
+++ test_zapisu.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 16:42:47 2020
+
+@author: User
+"""
+
+
+from symulacja import Populacja
+
+pop = Populacja(7)
+pop.zapis("plik.txt")
