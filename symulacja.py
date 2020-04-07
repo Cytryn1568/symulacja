c93dcf85a7843a74026d8d68b2963bad8c60957a
JGarwola <jakub.garwola@gmail.com>
2020-04-07 17:26:56 +0200

    Zmieniłem działnie zarażania głównie nowy setter

--- symulacja.py
+++ symulacja.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """Program do symulacji rozprzestrzeniania choroby w populacji
 Kod do wykorzystania na zajęciach 01.04.2020
@@ -5,36 +6,64 @@ Kod do wykorzystania na zajęciach 01.04.2020
 
 import random
 from math import sqrt
-
+    
 class Pacjent:
     """Pojedyncza osoba w symulacji. 
     
     Atrybuty:
         x (float): współrzędna x pozycji Pacjenta
         y (float): współrzędna y pozycji Pacjenta
-        status (str): status ze zbioru 'zdrowy', 'chory', 'nosiciel'
+        v_x (float): prędkość Pacjenta w kierunku x
+        v_y (float): prędkość Pacjenta w kierunku y
+        status (str): status ze zbioru 'zdrowy', 'chory', 'nosiciel'"""
     
-    """
-    
-    def __init__(self, x=0, y=0, czy_zdrowy=True):
+    def __init__(self, x=0, y=0, v_x = 0, v_y = 0, status = 'zdrowy'):
         self.x = x
         self.y = y
-        if czy_zdrowy:
-            self.status = 'zdrowy'
-        else:
-            self.status = 'chory'
+        self.v_x = v_x
+        self.v_y = v_y
+        self.set_status(status)
+        
+    def get_status(self):
+        return self.set_status
+    
+    def set_status(self,wartosc):
+        """ uzytkownik programu moze nadac pacjentowi tylko status z listy """
+        if wartosc not in ['zdrowy', 'chory', 'nosiciel']:
+            raise ValueError("Nie ma takiego statusu")
+        self.status = wartosc
             
-            
+    #@property
+    #def status(self):
+        #return self.status
+    
+    #@status.setter
+    #def status(self, wartosc):
+        #if wartosc not in ['zdrowy', 'chory', 'nosiciel']:
+            #raise ValueError("Nie ma takiego statusu")
+        #else:
+            #self.status = wartosc
+        
     def ruch(self):
         """Wykonaj ruch zmieniając współrzędne x,y.
-        
-        Zdrowy pacjent przesuwa się o 0-1, a chory o 0-0.1"""
+        Zdrowy pacjent przesuwa się z prędkością  
+        z zakresu z 0-1 i przyspieszeniem z 0-0.1, a chory z prędkością z 0-0.1 
+        i przyspieszeniem z 0-0.01."""
+      
         if self.status == 'chory':
-            zasieg = 0.1
+            predkosc_max = 0.1
         else:
-            zasieg = 1
-        self.x = self.x + random.uniform(-zasieg, zasieg)
-        self.y = self.y + random.uniform(-zasieg, zasieg)
+            predkosc_max = 1
+        zmiana_v_x = random.uniform(-predkosc_max/10, predkosc_max/10)
+        zmiana_v_y = random.uniform(-predkosc_max/10, predkosc_max/10)
+        if (self.v_x + zmiana_v_x)**2 + (self.v_y + zmiana_v_y)**2 > predkosc_max**2:
+            self.v_x = self.v_x
+            self.v_y = self.v_y
+        else:
+            self.v_x = self.v_x + zmiana_v_x 
+            self.v_y = self.v_y + zmiana_v_y
+        self.x = self.x + self.v_x
+        self.y = self.y + self.v_y
         
         
     def __str__(self):
@@ -65,8 +94,16 @@ class Populacja:
         for i in range(n):
             x = random.uniform(0, szerokosc)
             y = random.uniform(0, wysokosc)
-            zdrowy = random.choices( [True, False], [80, 20] )[0]
+            
+            zdrowy = random.choices( ['zdrowy', 'chory'], [80, 20] )[0]
             self._pacjenci.append( Pacjent(x, y, zdrowy) )
+            if self.status == 'chory':
+                predkosc_max = 0.1
+            else:
+                predkosc_max = 1
+            v_x = random.uniform(-predkosc_max, predkosc_max)
+            v_y = random.uniform(-sqrt(predkosc_max**2 - v_x**2), sqrt(predkosc_max**2 - v_x**2))
+            self._pacjenci.append( Pacjent(x, y, v_x, v_y, zdrowy) )
     
     def __str__(self):
         s = ""
@@ -88,11 +125,23 @@ class Populacja:
         for p in self._pacjenci:
             if p.status == 'zdrowy':
                 for p2 in self._pacjenci:
-                    zarazi = random.choices( [True, False], [1, 99] )[0]
+                    zarazi = random.choices( [True, False], [25, 75] )[0]
                     dystans = sqrt( (p.x - p2.x)**2 + (p.y - p2.y)**2 )
-                    if dystans>0 and dystans<5 and p2.status == 'chory' and zarazi:
-                        p.status = 'chory'
+                    if dystans>0 and dystans<15 and p2.status == 'chory' and zarazi:
+                        p.set_status = 'chory'
                         break
+                    
+    def zmiana_wymiarow(self, wysokosc, szerokosc):
+        """ Podaj nową wysokosc i szerokosc szpitala - zmienimy jego wymiary.
+        Jesli którys z pacjentów znajduje się poza jego granicami,
+        sprowadzimy go na granicę."""
+        for p in self._pacjenci:
+            if p.x>szerokosc:
+                p.x=float(szerokosc)
+            if p.y>wysokosc:
+                p.y=float(wysokosc)
+        self.wysokosc=wysokosc
+        self.szerokosc=szerokosc
     
     def zapisz(self,nazwa_pliku):
         fout = open(nazwa_pliku,'w')
---
+++ test_zmiany_statusu.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 14:39:30 2020
+
+@author: User
+"""
+
+
+import symulacja
+
+p = Pacjent(10,15,'zdrowy')
+print(p)
+p.status = 'chory'
+print(p)
+p.set_status = 'costam'
+print(p)
